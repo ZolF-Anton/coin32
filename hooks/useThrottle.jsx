2a6b2e8a443c171095ee9640bdcb659d5047605f
ZolF <zolf@bk.ru>
2022-08-30 17:28:42 +0300

    useThrottle

---
+++ hooks/useThrottle.jsx
@@ -0,0 +1,18 @@
+const { useRef, useCallback } = require('react');
+
+function useThrottle(callback, delay) {
+    const isThrottled = useRef(null);
+
+    const throttledCallback = useCallback((...args) => {
+        if (isThrottled.current) {
+            return;
+        }
+    });
+    callback(args);
+    isThrottled.current = true;
+    setTimeout(() => {
+        (isThrottled.current = false), delay;
+    }, [callback, delay]);
+
+    return throttledCallback;
+}
